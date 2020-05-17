d5d2d5671003ff902fa44c324c0deb8920bf8332
Nkosinathi-Bonga-James-Mncube <nmncube@student.wethinkcode.co.za>
2020-05-18 00:14:41 +0200

    Norming file for ft_path_man

--- src/ft_path_man.c
+++ src/ft_path_man.c
@@ -12,50 +12,48 @@
 
 #include "../inc/minishell.h"
 
-char *get_path(char **environ)
+char	*get_path(char **environ)
 {
-    char *found;
-    char **paths;
-    int i;
+	char	*found;
+	char	**paths;
+	int	i;
 
-    paths = NULL;
-    i = 0;
-    while (environ[i])
-    {
-        if (compar(environ[i], "PATH") == 0)
-            found = ft_strdup(environ[i]);
-        i++;
-    }
-    if (found)
-    {
-        paths = ft_strsplit(found, ':');
-        i = 0;
-        free(found);
-        while (paths[i])
-        {
-            if (ft_strcmp("/bin", paths[i]) == 0 || ft_strcmp("PATH=/bin", paths[i]) == 0)
-                found = ft_strdup(paths[++i]);
-            i++;
-        }
-        free_(paths);
-        return (found);
-    }
-    return (NULL);
+	paths = NULL;
+	i = 0;
+	while (environ[i])
+	{
+		if (compar(environ[i], "PATH") == 0)
+			found = ft_strdup(environ[i]);
+		i++;
+	}
+	if (found)
+	{
+		paths = ft_strsplit(found, ':');
+		i = 0;
+		free(found);
+		while (paths[i])
+		{
+			if (ft_strcmp("/bin", paths[i]) == 0 || ft_strcmp("PATH=/bin", paths[i]) == 0)
+				found = ft_strdup(paths[++i]);
+			i++;
+		}
+		free_(paths);
+		return (found);
+	}
+	return (NULL);
 }
 
-int final_path(char **command, char **envp)
+int	final_path(char **command, char **envp)
 {
-    // char *path;
-    char *s = NULL;
-
-    g_path_ = get_path(envp);
-    if (g_path_)
-    {
-        s = ft_strjoin(g_path_, "/");
-        free(g_path_);
-        g_path_ = ft_strjoin(s, command[0]);
-        free(s);
-        return (0);
-    }
-    return (-1);
+	char	*s = NULL;
+	g_path_ = get_path(envp);
+	if (g_path_)
+	{
+		s = ft_strjoin(g_path_, "/");
+		free(g_path_);
+		g_path_ = ft_strjoin(s, command[0]);
+		free(s);
+		return (0);
+	}
+	return (-1);
 }
