10f6bc26badc3e77156ea562f8463834ebf05a0e
Adonis <apple@apples-MacBook-Air.local>
2020-05-17 14:41:41 +0200

    command line management done

--- src/pars.c
+++ src/pars.c
@@ -6,7 +6,7 @@
 /*   By: akalombo <akalombo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/29 14:25:26 by akalombo          #+#    #+#             */
-/*   Updated: 2020/05/17 12:04:01 by akalombo         ###   ########.fr       */
+/*   Updated: 2020/05/17 14:33:54 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,20 @@ int built_ins(char *str)
         return (-1);
     else if (ft_strcmp("setenv", str) != 0 || ft_strcmp("env", str) != 0)
         return (-1);
+    return (0);
+}
+
+int check_(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] > 32)
+            return (-1);
+        i++;
+    }
     return (0);
 }
 
@@ -67,21 +81,24 @@ size_t crete_process(pid_t child, char **command, char *lineptr, char *c, char *
         ft_putstr("$> ");
         if (getline(&lineptr, &status, stdin) == -1)
             break;
-        if (ft_strlen(lineptr) != 1)
+        if (check_(lineptr) == -1)
         {
-            c = lineptr;
-            lineptr = ft_strtrim(lineptr);
-            free(c);
-            command = ft_strsplit(lineptr, ' '); 
-        }
-        child = fork();
-        process(child, command, envp, status, lineptr);
-        status = 0;
-        if (ft_strlen(lineptr) > 1 && built_ins(command[0]) == -1)
-        {
-            while (command[status])
-                free(command[status++]);
-            free(command);
+            if (ft_strlen(lineptr) != 1)
+            {
+                c = lineptr;
+                lineptr = ft_strtrim(lineptr);
+                free(c);
+                command = ft_strsplit(lineptr, ' '); 
+            }
+            child = fork();
+            process(child, command, envp, status, lineptr);
+            status = 0;
+            if (ft_strlen(lineptr) > 1 && built_ins(command[0]) == -1)
+            {
+                while (command[status])
+                    free(command[status++]);
+                free(command);
+            }
         }
     }
     return (status);
@@ -125,18 +142,18 @@ void process(pid_t child, char **command, char **envp, int status, char *ptr)
         }
         if (g_path_ != NULL)
             free(g_path_);
-         final_path(command, envp);
+        final_path(command, envp);
         if (echo_(command, ptr) == -1 && ft_strlen(ptr) != 1)
         {
             if (ch_dir(command) == -1 && call_env(ptr) == -1 && env_(command[0]) == -1 && unset(ptr) == -1)
             {
-            if (execve(command[0], command, envp)  == -1 && execve(g_path_, command, envp) == -1)
-            {
-                ft_putstr("minishell: command not found: ");
-                ft_putstr(ptr);
-                ft_putchar('\n');
-                exit(EXIT_FAILURE);
-            }
+                if (execve(command[0], command, envp)  == -1 && execve(g_path_, command, envp) == -1)
+                {
+                    ft_putstr("minishell: command not found: ");
+                    ft_putstr(ptr);
+                    ft_putchar('\n');
+                    exit(EXIT_FAILURE);
+                }
             }
         }
     }
